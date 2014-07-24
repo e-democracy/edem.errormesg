7ce30f13a22cf0d5543aef13fb49915579fc4fff
Bill Bushey <wbushey@gmail.com>
2014-07-23 19:01:07 -0500

    Initial commit

---
+++ README.txt
@@ -0,0 +1,34 @@
+==============================
+``edem.errormesg``
+==============================
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+E-Democracy's version of the error and not_found pages
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+:Author: `Bill Bushey`_
+:Contact: Bill Bushey <techteam@e-democracy.org>
+:Date: 2014-07-23
+:Organization: `E-Democracy`_
+:Copyright: This document is licensed under a
+  `Creative Commons Attribution-Share Alike 3.0 License`_
+  by `E-Democracy`_.
+
+Introduction
+===========
+
+This product provides E-Democracy's Not Found and Unexpected Error pages. It
+is based similar pages for `GroupServer`_ provided by `gs.errormesg`_
+
+Resources
+=========
+
+- Code repository: https://github.com/e-democracy/edem.errormesg
+- Questions and comments to http://groupserver.org/groups/development
+- Report bugs at https://redmine.iopen.net/projects/edem
+
+.. _GroupServer: http://groupserver.org/
+.. _E-Democracy: http://e-democracy.org/
+.. _Bill Bushey: http://groupserver.org/p/wbushey
+.. _gs.errormesg: https://source.iopen.net/groupserver/gs.errormesg
+.. _Creative Commons Attribution-Share Alike 3.0 License:
+   http://creativecommons.org/licenses/by-sa/3.0/
---
+++ setup.py
@@ -0,0 +1,45 @@
+# -*- coding=utf-8 -*-
+from __future__ import absolute_import, unicode_literals
+import os
+from setuptools import setup, find_packages
+from version import get_version
+
+version = get_version()
+
+setup(
+    name='edem.errormesg',
+    version=version,
+    description="E-Democracy's version of the error and not_found pages",
+    long_description=open("README.txt").read() + "\n" +
+    open(os.path.join("docs", "HISTORY.txt")).read(),
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers for values
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
+        "Framework :: Zope2",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License v3 or later "
+        "(GPLv3+)",
+        "Natural Language :: English",
+        "Operating System :: POSIX :: Linux"
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    keywords='',
+    author='Bill Bushey',
+    author_email='techteam@e-democracy.org',
+    url='http://www.e-democracy.org/',
+    license='GPL 3',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['edem'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'edem.skin',
+        'gs.errormesg',
+        # -*- Extra requirements: -*-
+    ],
+    entry_points="""
+    # -*- Entry points: -*-
+    """,)
